826df445662ed51ad45b6ed2666a6aabf939450b
tayyip90 <38054007+tayyip90@users.noreply.github.com>
2020-09-06 13:14:52 +0200

    + Classdiagram created

---
+++ ChessGame/ChessGame/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame
+{
+    public class Game
+    {
+        private ChessLogic logic;
+
+        public Game()
+        {
+            logic = new ChessLogic();
+        }
+    }
+}
